4d871fd2cb163e09f264c739e1efa7c4366db585
𝚴 𝐎 𝐁 𝚰 𝐓 𝚲 <nobitaxmusicbot@gmail.com>
2025-08-04 13:41:14 +0530

    Update help.py

--- Oneforall/utils/inline/help.py
+++ Oneforall/utils/inline/help.py
@@ -104,7 +104,7 @@ def help_pannel(_, START: Union[bool, int] = None):
                     callback_data="help_callback hb16",
                 ),
                 InlineKeyboardButton(
-                    "fun game",
+                    "⚘ Fᴜɴ Gᴀᴍᴇ ➻",
                     callback_data="help_callback hb21",
                 ),
             ],
@@ -118,7 +118,7 @@ def help_pannel(_, START: Union[bool, int] = None):
                     callback_data="help_callback hb19",
                 ),
                 InlineKeyboardButton(
-                    "fsub",
+                    "⚘ Fsᴜʙ ➻",
                     callback_data="help_callback hb20",
                 ),
             ],
