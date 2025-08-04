112480f118c30f793fa4de6083971ba1713e26e4
𝚴 𝐎 𝐁 𝚰 𝐓 𝚲 <nobitaxmusicbot@gmail.com>
2025-08-04 13:38:01 +0530

    Update buttons.py

--- Oneforall/utils/stuffs/buttons.py
+++ Oneforall/utils/stuffs/buttons.py
@@ -4,34 +4,34 @@ from pyrogram.types import InlineKeyboardButton
 class BUTTONS(object):
     MBUTTON = [
         [
-            InlineKeyboardButton("CʜᴀᴛGPT", callback_data="mplus HELP_ChatGPT"),
-            InlineKeyboardButton("Hɪsᴛᴏʀʏ", callback_data="mplus HELP_History"),
-            InlineKeyboardButton("Rᴇᴇʟ", callback_data="mplus HELP_Reel"),
+            InlineKeyboardButton("⚘ Cʜᴀᴛ-Gᴘᴛ ➻", callback_data="mplus HELP_ChatGPT"),
+            InlineKeyboardButton("⚘ Hɪsᴛᴏʀʏ ➻", callback_data="mplus HELP_History"),
+            InlineKeyboardButton("⚘ Rᴇᴇʟ ➻", callback_data="mplus HELP_Reel"),
         ],
         [
-            InlineKeyboardButton("Tᴀɢ-Aʟʟ", callback_data="mplus HELP_TagAll"),
-            InlineKeyboardButton("Iɴꜰᴏ", callback_data="mplus HELP_Info"),
-            InlineKeyboardButton("Exᴛʀᴀ", callback_data="mplus HELP_Extra"),
+            InlineKeyboardButton("⚘ Tᴀɢ-Aʟʟ ➻", callback_data="mplus HELP_TagAll"),
+            InlineKeyboardButton("⚘ Iɴꜰᴏ ➻", callback_data="mplus HELP_Info"),
+            InlineKeyboardButton("⚘ Exᴛʀᴀ ➻", callback_data="mplus HELP_Extra"),
         ],
         [
-            InlineKeyboardButton("ᴄᴏᴜᴘʟᴇꜱ", callback_data="mplus HELP_Couples"),
-            InlineKeyboardButton("Aᴄᴛɪᴏɴ", callback_data="mplus HELP_Action"),
-            InlineKeyboardButton("Sᴇᴀʀᴄʜ", callback_data="mplus HELP_Search"),
+            InlineKeyboardButton("⚘ Cᴏᴜᴘʟᴇꜱ ➻", callback_data="mplus HELP_Couples"),
+            InlineKeyboardButton("⚘ Aᴄᴛɪᴏɴ ➻", callback_data="mplus HELP_Action"),
+            InlineKeyboardButton("⚘ Sᴇᴀʀᴄʜ ➻", callback_data="mplus HELP_Search"),
         ],
         [
-            InlineKeyboardButton("ғᴏɴᴛ", callback_data="mplus HELP_Font"),
-            InlineKeyboardButton("Bᴏᴛs", callback_data="mplus HELP_Bots"),
-            InlineKeyboardButton("Ⓣ-ɢʀᴀᴘʜ", callback_data="mplus HELP_TG"),
+            InlineKeyboardButton("⚘ Fᴏɴᴛ ➻", callback_data="mplus HELP_Font"),
+            InlineKeyboardButton("⚘ Bᴏᴛs ➻", callback_data="mplus HELP_Bots"),
+            InlineKeyboardButton("⚘ Ⓣ-ɢʀᴀᴘʜ ➻", callback_data="mplus HELP_TG"),
         ],
         [
-            InlineKeyboardButton("Sᴏᴜʀᴄᴇ", callback_data="mplus HELP_Source"),
-            InlineKeyboardButton("Tʀᴜᴛʜ-ᗪᴀʀᴇ", callback_data="mplus HELP_TD"),
-            InlineKeyboardButton("Qᴜɪᴢ", callback_data="mplus HELP_Quiz"),
+            InlineKeyboardButton("⚘ Sᴏᴜʀᴄᴇ ➻", callback_data="mplus HELP_Source"),
+            InlineKeyboardButton("⚘ Tʀᴜᴛʜ-ᗪᴀʀᴇ ➻", callback_data="mplus HELP_TD"),
+            InlineKeyboardButton("⚘ Qᴜɪᴢ ➻", callback_data="mplus HELP_Quiz"),
         ],
         [
-            InlineKeyboardButton("ᴛᴛs", callback_data="mplus HELP_TTS"),
-            InlineKeyboardButton("Rᴀᴅɪᴏ", callback_data="mplus HELP_Radio"),
-            InlineKeyboardButton("ǫᴜᴏᴛʟʏ", callback_data="mplus HELP_Q"),
+            InlineKeyboardButton("⚘ Tᴛs ➻", callback_data="mplus HELP_TTS"),
+            InlineKeyboardButton("⚘ Rᴀᴅɪᴏ ➻", callback_data="mplus HELP_Radio"),
+            InlineKeyboardButton("⚘ Qᴜᴏᴛʟʏ ➻", callback_data="mplus HELP_Q"),
         ],
         [
             InlineKeyboardButton("◁", callback_data=f"settings_back_helper"),
