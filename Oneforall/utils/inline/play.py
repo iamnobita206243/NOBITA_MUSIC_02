8e96a7fbb060efee7dd774db1c4a08b2ed5f230d
𝚴 𝐎 𝐁 𝚰 𝐓 𝚲 <nobitaxmusicbot@gmail.com>
2025-08-04 16:09:29 +0530

    Update play.py

--- Oneforall/utils/inline/play.py
+++ Oneforall/utils/inline/play.py
@@ -63,11 +63,11 @@ def stream_markup_timer(_, vidid, chat_id, played, dur):
             )
         ],
         [
-            InlineKeyboardButton(text="⏸", callback_data=f"ADMIN Pause|{chat_id}"),
-            InlineKeyboardButton(text="▶️", callback_data=f"ADMIN Resume|{chat_id}"),
+            InlineKeyboardButton(text="▷", callback_data=f"ADMIN Pause|{chat_id}"),
+            InlineKeyboardButton(text="II", callback_data=f"ADMIN Resume|{chat_id}"),
             InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
             InlineKeyboardButton(text="⏭", callback_data=f"ADMIN Skip|{chat_id}"),
-            InlineKeyboardButton(text="⏹", callback_data=f"ADMIN Stop|{chat_id}")
+            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}")
         ],
         [
             InlineKeyboardButton(
