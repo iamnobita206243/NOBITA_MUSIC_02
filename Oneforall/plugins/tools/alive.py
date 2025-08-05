aad5c71bdf9a965df74b85f36b4a8e5773b03529
iamnobita206243 <nobitamusic001@gmail.com>
2025-08-05 15:59:50 +0530

    Update alive.py

--- Oneforall/plugins/tools/alive.py
+++ Oneforall/plugins/tools/alive.py
@@ -7,25 +7,32 @@ from Oneforall import app
 
 @app.on_message(filters.command(["alive"]))
 async def start(client: Client, message: Message):
+    bot = await app.get_me()
+    mention = f"[{bot.first_name}](https://t.me/{bot.username})"
+
     await message.reply_video(
-        video=f"https://files.catbox.moe/l8duqz.jpg",
-        caption=f"❤️ ʜᴇʏ {message.from_user.mention}\n\n🔮 ɪ ᴀᴍ {MUSIC_BOT_NAME}\n\n✨ ɪ ᴀᴍ ғᴀsᴛ ᴀɴᴅ ᴩᴏᴡᴇʀғᴜʟ ᴍᴜsɪᴄ ᴩʟᴀʏᴇʀ ʙᴏᴛ ᴡɪᴛʜ sᴏᴍᴇ ᴀᴡᴇsᴏᴍᴇ ғᴇᴀᴛᴜʀᴇs.\n\n💫 ɪғ ʏᴏᴜ ʜᴀᴠᴇ ᴀɴʏ ǫᴜᴇsᴛɪᴏɴs ᴛʜᴇɴ ᴊᴏɪɴ ᴏᴜʀ sᴜᴘᴘᴏʀᴛ ɢʀᴏᴜᴘ🤍...\n\n━━━━━━━━━━━━━━━━━━❄",
+        video="https://files.catbox.moe/l8duqz.jpg",
+        caption=f"""❤️ HEY {message.from_user.mention}\n\n🤖 I AM {mention}\n\n✨ I AM FAST AND POWERFUL MUSIC PLAYER BOT WITH SOME AWESOME FEATURES.\n\n💫 IF YOU HAVE ANY QUESTIONS THEN JOIN OUR SUPPORT GROUP🤍 ...
+""",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        text="☆ ᴏᴡɴᴇʀ ☆", url=f"https://t.me/ll_NOBITA_DEFAULTERS_ll"
+                        text="☆ OWNER ☆", url="https://t.me/ll_NOBITA_DEFAULTERS_ll"
                     ),
                     InlineKeyboardButton(
-                        text="☆ ꜱᴜᴘᴘᴏʀᴛ ☆", url=f"https://t.me/+WLTHgUAvkYVmNTg9"
+                        text="☆ SUPPORT ☆", url="https://t.me/+WLTHgUavkYVmNtg9"
                     ),
                 ],
                 [
                     InlineKeyboardButton(
-                        text="☆ ᴜᴘᴅᴀᴛᴇ ☆", url=f"https://t.me/NOB1TA_SUPPORT"
+                        text="☆ UPDATE ☆", url="https://t.me/NOBITA_SUPPORT"
                     ),
-                ],
-                [InlineKeyboardButton("✯ ᴄʟᴏsᴇ ✯", callback_data="close")],
+                    InlineKeyboardButton(
+                        text="★ CLOSE ★", callback_data="close"
+                    ),
+                ]
             ]
         ),
+        parse_mode="markdown"
     )
