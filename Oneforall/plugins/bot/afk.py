bb30595436e9910ae49df7a39aee7cfe28ad769b
iamnobita206243 <nobitamusic001@gmail.com>
2025-08-06 20:12:48 +0530

    Update afk.py

--- Oneforall/plugins/bot/afk.py
+++ Oneforall/plugins/bot/afk.py
@@ -1,44 +1,26 @@
-import re
-import time
-
-from pyrogram import filters
+import time, re
+from config import BOT_USERNAME
 from pyrogram.enums import MessageEntityType
+from pyrogram import filters
 from pyrogram.types import Message
-
 from Oneforall import app
-from Oneforall.mongo.afkdb import add_afk, is_afk, remove_afk
 from Oneforall.mongo.readable_time import get_readable_time
-
+from Oneforall.mongo.afkdb import add_afk, is_afk, remove_afk
 
 @app.on_message(filters.command(["afk", "brb"], prefixes=["/", "!"]))
 async def active_afk(_, message: Message):
     if message.sender_chat:
         return
     user_id = message.from_user.id
-    verifier, reasondb = await is_afk(user_id)
-    if verifier:
-        await remove_afk(user_id)
-        timeafk = reasondb["time"]
-        reasonafk = reasondb["reason"]
-        seenago = get_readable_time(int(time.time() - timeafk))
 
-        if reasonafk:
-            await message.reply_text(
-                f"**{message.from_user.first_name}** is back online and was away for {seenago}\n\nReason: `{reasonafk}`"
-            )
-        else:
-            await message.reply_text(
-                f"**{message.from_user.first_name}** is back online and was away for {seenago}"
-            )
-
-    if len(message.command) == 1:
+    if len(message.command) == 1 and not message.reply_to_message:
         details = {
             "type": "text",
             "time": time.time(),
             "data": None,
             "reason": None,
         }
-    else:
+    elif len(message.command) > 1 and not message.reply_to_message:
         _reason = (message.text.split(None, 1)[1].strip())[:100]
         details = {
             "type": "text_reason",
@@ -46,77 +28,90 @@ async def active_afk(_, message: Message):
             "data": None,
             "reason": _reason,
         }
+    else:
+        details = {
+            "type": "text",
+            "time": time.time(),
+            "data": None,
+            "reason": None,
+        }
 
     await add_afk(user_id, details)
-    await message.reply_text(f"{message.from_user.first_name} is now AFK.")
+    await message.reply_text(f"{message.from_user.first_name} ɪs ɴᴏᴡ ᴀғᴋ!")
 
+chat_watcher_group = 1
 
-@app.on_message(~filters.me & ~filters.bot & ~filters.via_bot, group=1)
+@app.on_message(
+    ~filters.me & ~filters.bot & ~filters.via_bot,
+    group=chat_watcher_group,
+)
 async def chat_watcher_func(_, message):
     if message.sender_chat:
         return
-
-    user_id = message.from_user.id
+    userid = message.from_user.id
     user_name = message.from_user.first_name
     msg = ""
+    replied_user_id = 0
 
-    # Remove AFK if user sends message
-    verifier, reasondb = await is_afk(user_id)
-    if verifier:
-        await remove_afk(user_id)
-        timeafk = reasondb["time"]
-        reasonafk = reasondb["reason"]
-        seenago = get_readable_time(int(time.time() - timeafk))
+    # Ignore AFK check if command is /afk or /brb
+    if message.text and message.text.split()[0].lower() not in ["/afk", f"/afk@{BOT_USERNAME}", "/brb", f"/brb@{BOT_USERNAME}"]:
+        verifier, reasondb = await is_afk(userid)
+        if verifier:
+            await remove_afk(userid)
+            try:
+                afktype = reasondb["type"]
+                timeafk = reasondb["time"]
+                reasonafk = reasondb["reason"]
+                seenago = get_readable_time((int(time.time() - timeafk)))
+                if afktype == "text":
+                    msg += f"**{user_name[:25]}** ɪs ʙᴀᴄᴋ ᴏɴʟɪɴᴇ ᴀɴᴅ ᴡᴀs ᴀᴡᴀʏ ғᴏʀ {seenago}\n\n"
+                if afktype == "text_reason":
+                    msg += f"**{user_name[:25]}** ɪs ʙᴀᴄᴋ ᴏɴʟɪɴᴇ ᴀɴᴅ ᴡᴀs ᴀᴡᴀʏ ғᴏʀ {seenago}\n\nʀᴇᴀsᴏɴ: `{reasonafk}`\n\n"
+            except:
+                msg += f"**{user_name[:25]}** ɪs ʙᴀᴄᴋ ᴏɴʟɪɴᴇ\n\n"
 
-        if reasonafk:
-            msg += f"**{user_name}** is back online and was away for {seenago}\n\nReason: `{reasonafk}`\n\n"
-        else:
-            msg += f"**{user_name}** is back online and was away for {seenago}\n\n"
-
-    # Check if replying to AFK user
+    # Handle reply AFK detection
     if message.reply_to_message:
         try:
-            r_user = message.reply_to_message.from_user
-            r_verifier, r_reasondb = await is_afk(r_user.id)
-            if r_verifier:
-                r_timeafk = r_reasondb["time"]
-                r_reasonafk = r_reasondb["reason"]
-                r_seenago = get_readable_time(int(time.time() - r_timeafk))
-                if r_reasonafk:
-                    msg += f"**{r_user.first_name}** is AFK since {r_seenago}\n\nReason: `{r_reasonafk}`\n\n"
-                else:
-                    msg += f"**{r_user.first_name}** is AFK since {r_seenago}\n\n"
+            replied_first_name = message.reply_to_message.from_user.first_name
+            replied_user_id = message.reply_to_message.from_user.id
+            verifier, reasondb = await is_afk(replied_user_id)
+            if verifier:
+                afktype = reasondb["type"]
+                timeafk = reasondb["time"]
+                reasonafk = reasondb["reason"]
+                seenago = get_readable_time((int(time.time() - timeafk)))
+                if afktype == "text":
+                    msg += f"**{replied_first_name[:25]}** ɪs ᴀғᴋ sɪɴᴄᴇ {seenago}\n\n"
+                if afktype == "text_reason":
+                    msg += f"**{replied_first_name[:25]}** ɪs ᴀғᴋ sɪɴᴄᴇ {seenago}\n\nʀᴇᴀsᴏɴ: `{reasonafk}`\n\n"
         except:
             pass
 
-    # Check for mentions
+    # Detect plain @username mentions in text
     if message.entities:
-        entity = message.entities
-        for i in range(len(entity)):
-            ent = entity[i]
+        text = message.text or message.caption or ""
+        mentions = re.findall(r"@([_0-9a-zA-Z]+)", text)
+        for mention in mentions:
             try:
-                if ent.type == MessageEntityType.MENTION:
-                    username = re.findall("@([_0-9a-zA-Z]+)", message.text)[i]
-                    user = await app.get_users(username)
-                elif ent.type == MessageEntityType.TEXT_MENTION:
-                    user = ent.user
-                else:
+                user = await app.get_users(mention)
+                if user.id == userid:
                     continue
-
-                if user.id == message.reply_to_message.from_user.id:
-                    continue
-
-                v, db = await is_afk(user.id)
-                if v:
-                    t = db["time"]
-                    r = db["reason"]
-                    sa = get_readable_time(int(time.time() - t))
-                    if r:
-                        msg += f"**{user.first_name}** is AFK since {sa}\n\nReason: `{r}`\n\n"
-                    else:
-                        msg += f"**{user.first_name}** is AFK since {sa}\n\n"
+                verifier, reasondb = await is_afk(user.id)
+                if verifier:
+                    afktype = reasondb["type"]
+                    timeafk = reasondb["time"]
+                    reasonafk = reasondb["reason"]
+                    seenago = get_readable_time((int(time.time() - timeafk)))
+                    if afktype == "text":
+                        msg += f"**{user.first_name[:25]}** ɪs ᴀғᴋ sɪɴᴄᴇ {seenago}\n\n"
+                    if afktype == "text_reason":
+                        msg += f"**{user.first_name[:25]}** ɪs ᴀғᴋ sɪɴᴄᴇ {seenago}\n\nʀᴇᴀsᴏɴ: `{reasonafk}`\n\n"
             except:
                 continue
 
-    if msg:
-        await message.reply_text(msg, disable_web_page_preview=True)
+    if msg != "":
+        try:
+            await message.reply_text(msg, disable_web_page_preview=True)
+        except:
+            return
