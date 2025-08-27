ba0198419aaf4e6885d272da4f390d4e98bd505a
iamnobita206243 <nobitamusic001@gmail.com>
2025-08-27 17:50:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -36,9 +36,6 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 BASE_API_URL = "https://zyro.zyronetworks.shop"
-BASE_API_KEY = "CeXY5r3HH3yyPMICJ93FfgzCI12C6fzn"
-
-BASE_API_URL = "https://zyro.zyronetworks.shop"
 BASE_API_KEY = "N9F81n1ZDxiKo4gsJsiQXsv7m5U7IPx2"
 
 UPSTREAM_REPO = getenv(
